1ee1b729e87c7eab9cfef06d5829bfa844c20437
Asmeera <asmeeranadeem22@gmail.com>
2026-09-03 22:56:17 +0500

    completed project 1

---
+++ javascript project1.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+   
+    buttons.forEach(button =>{
+         button.addEventListener("click", ()=>{
+
+            if(button.id=== "1"){
+                body.style.backgroundColor = "grey";
+            } else if(button.id === "2"){
+                body.style.backgroundColor = "yellow";
+            } else if(button.id === "3"){
+                body.style.backgroundColor = "blue";
+            } else if(button.id === "4"){
+                body.style.backgroundColor = "green";
+            }
+           
+        });
+
+   
+    });
+    
