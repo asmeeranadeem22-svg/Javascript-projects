df4aefa4efd96d34cf227b1c0ebb636723c9526f
Asmeera <asmeeranadeem22@gmail.com>
2026-09-03 23:58:09 +0500

    Refactor color switching logic to store original background color

--- javascript project1.js
+++ javascript project1.js
@@ -1,10 +1,11 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
-
+const originalColor = body.style.backgroundColor;
    
     buttons.forEach(button =>{
+             
          button.addEventListener("click", ()=>{
-
+          
             if(button.id=== "1"){
                 body.style.backgroundColor = "grey";
             } else if(button.id === "2"){
@@ -14,9 +15,11 @@ const body = document.querySelector("body");
             } else if(button.id === "4"){
                 body.style.backgroundColor = "green";
             }
-           
+            setTimeout(()=>{
+                body.style.backgroundColor = originalColor;
+            },2000);
         });
-
+ 
    
     });
     
